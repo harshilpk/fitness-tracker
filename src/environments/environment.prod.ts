34dbc2eab6ab69b616b024dd14d0c08f1df4f02e
Harshil <harshilpk@gmail.com>
2018-10-23 17:48:26 -0700

    Prouction level changes. To ensure this updated angular to 7.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyByNKDHb6_A_FQH83qzFBn0ZasKxQ8WjQQ',
+    authDomain: 'ng-fitness-tracker-f3fee.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-f3fee.firebaseio.com',
+    projectId: 'ng-fitness-tracker-f3fee',
+    storageBucket: 'ng-fitness-tracker-f3fee.appspot.com',
+    messagingSenderId: '206992352164'
+  }
 };
